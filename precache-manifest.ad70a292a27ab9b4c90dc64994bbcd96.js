8e062a28fcd7963c9c52fa13c20a655a66e9e3cc
tmnrwd <61465712+tmnrwd@users.noreply.github.com>
2020-10-10 21:51:42 +0100

    Updates

---
+++ precache-manifest.ad70a292a27ab9b4c90dc64994bbcd96.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ef378fa65055e3b3426c69e2ca0a1f7",
+    "url": "/dice-roller/index.html"
+  },
+  {
+    "revision": "5d44b3bbcc6f78b1ab1f",
+    "url": "/dice-roller/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "f3c829ab57953df4b02f",
+    "url": "/dice-roller/static/css/main.bb1fa08a.chunk.css"
+  },
+  {
+    "revision": "5d44b3bbcc6f78b1ab1f",
+    "url": "/dice-roller/static/js/2.c6b0f9fb.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/dice-roller/static/js/2.c6b0f9fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3c829ab57953df4b02f",
+    "url": "/dice-roller/static/js/main.c3e6944d.chunk.js"
+  },
+  {
+    "revision": "1fef78c38f42099e7f04",
+    "url": "/dice-roller/static/js/runtime-main.f9aa7fc1.js"
+  }
+]);
