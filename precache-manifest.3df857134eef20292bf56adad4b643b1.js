bbf2eeba1ded51a3048467c61e7c912d171e3114
tmnrwd <61465712+tmnrwd@users.noreply.github.com>
2020-10-17 01:15:40 +0100

    Updates

--- precache-manifest.3df857134eef20292bf56adad4b643b1.js
+++ precache-manifest.3df857134eef20292bf56adad4b643b1.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "30683b9e6494a68a71510f6dd45b82c1",
+    "revision": "90d1936fb17db4e284134fc1a339071a",
     "url": "/dice-roller/index.html"
   },
   {
-    "revision": "3b57149ba30c579750b2",
+    "revision": "da307864543b4f24df93",
     "url": "/dice-roller/static/css/2.d34346ea.chunk.css"
   },
   {
-    "revision": "7cca83cd8ed346b3db64",
+    "revision": "6a21228574f903ff1d85",
     "url": "/dice-roller/static/css/main.72f20fee.chunk.css"
   },
   {
-    "revision": "3b57149ba30c579750b2",
-    "url": "/dice-roller/static/js/2.e09bbe38.chunk.js"
+    "revision": "da307864543b4f24df93",
+    "url": "/dice-roller/static/js/2.7ec4bd75.chunk.js"
   },
   {
     "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
-    "url": "/dice-roller/static/js/2.e09bbe38.chunk.js.LICENSE.txt"
+    "url": "/dice-roller/static/js/2.7ec4bd75.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7cca83cd8ed346b3db64",
-    "url": "/dice-roller/static/js/main.5103413a.chunk.js"
+    "revision": "6a21228574f903ff1d85",
+    "url": "/dice-roller/static/js/main.9b001c4a.chunk.js"
   },
   {
     "revision": "1fef78c38f42099e7f04",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dice-roller/precache-manifest.e2d5acda0a7b81d42f5949e28580c70d.js"
+  "/dice-roller/precache-manifest.3df857134eef20292bf56adad4b643b1.js"
 );
 
 self.addEventListener('message', (event) => {
